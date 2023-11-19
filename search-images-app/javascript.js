ba9cc20e3bc1548a26cf3be3bbcedad04a5deb52
ibrahim-shedo <135816154+ibrahim-shedo@users.noreply.github.com>
2023-11-19 13:56:25 +0300

    Add files via upload
    Certainly! Let's break down the code step by step:
    
    ### 1. Variables:
    
    - **`accesskey`:**
      - This variable holds your Unsplash API access key. It's essential for making requests to the Unsplash API.
    
    - **DOM Element References:**
      - **`formEl`, `inputEl`, `searchResult`, `showMore`:**
        - These variables store references to various HTML elements using `document.querySelector` and `getElementById`. They correspond to the form, search input field, search results container, and the "Show More" button in your HTML.
    
    - **Other Variables:**
      - **`inputData`:**
        - This variable is initialized as an empty string and later assigned the value of the user's input from the search input field (`inputEl.value`).
    
      - **`page`:**
        - This variable keeps track of the current page of search results. It starts at 1 and is incremented after each search.
    
    ### 2. `searchImages` Function:
    
    - **Async Function:**
      - `async function searchImages() { ... }`: This is an asynchronous function used for fetching and displaying search results from the Unsplash API.
    
    - **API Request:**
      - `const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;`: This constructs the URL for the Unsplash API request, including the page number, search query, and API key.
    
    - **Fetch API:**
      - `const response = await fetch(url);`: It uses the `fetch` function to make an asynchronous request to the Unsplash API.
    
    - **JSON Parsing:**
      - `const data = await response.json();`: It asynchronously parses the JSON response from the API.
    
    - **Result Handling:**
      - `const results = data.results;`: Extracts the array of image results from the response.
    
    - **Clearing Previous Results:**
      - `if (page === 1) { searchResult.innerHTML = " "; }`: If it's the first page, it clears the existing content in the `searchResult` container.
    
    - **Creating HTML Elements:**
      - It iterates over the array of image results using `map` and creates HTML elements (image, link) for each result.
    
    - **Page Increment:**
      - `page++;`: Increments the `page` variable for the next search.
    
    - **Show More Button Display:**
      - `if (page > 1) { showMore.style.display = "block"; }`: Displays the "Show More" button if the page is greater than 1.
    
    ### 3. Event Listeners:
    
    - **Form Submission Event:**
      - `formEl.addEventListener("submit", (event) => { ... });`: Listens for the form submission event, prevents the default behavior, resets the `page` variable to 1, and calls the `searchImages` function.
    
    - **"Show More" Button Event:**
      - `showMore.addEventListener("click", () => { ... });`: Listens for the click event on the "Show More" button and calls the `searchImages` function to load more results.
    
    ### 4. HTML Structure:
    
    - The HTML structure includes a form with an input field, a button, and specific elements with IDs and classes that the JavaScript code references.
    
    ### Important Note:
    
    - Ensure that your HTML file includes the necessary elements and that the JavaScript file is included after the HTML elements in the document. Additionally, keep your Unsplash API key secure and avoid exposing it in client-side code for production applications.

---
+++ search-images-app/javascript.js
@@ -0,0 +1,60 @@
+const  accesskey = "S8nRu8b_bRA1xaS7UcmqRVcOe4CYsEUzcY1OeTt0fZE";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResult = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+ async function searchImages (){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;
+
+ 
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML=" ";
+    }
+    //creating container
+    results.map((result) =>{
+        const imageWrapper = document.createElement("div");
+        //naming class
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imagelink = document.createElement("a");
+        imagelink.href = result.links.html;
+        imagelink.target = "_blank";
+        imagelink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imagelink);
+        searchResult.appendChild(imageWrapper);
+    })
+
+    //increment the pages 
+ page++;
+ //displaying the show more button if the page is >1
+ if(page > 1){
+    showMore.style.display = "block";
+ }
+}
+
+formEl.addEventListener("submit", (event) =>{
+  event.preventDefault();
+  page = 1;
+  searchImages();
+
+})
+showMore.addEventListener("click", () =>{
+    
+    searchImages();
+  
+  })
